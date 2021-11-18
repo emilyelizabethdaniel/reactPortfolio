4360ae63b5709d04a69370c1b4fabae67559b15a
Basquiat <ememdaniel98@gmail.com>
2021-11-17 19:24:56 -0600

    added image and about me

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,13 +1,38 @@
 import React from "react";
-import Wires from "../images/wires.jpg";
+import profile from "../images/profile.jpg"
 import "../css/aboutMe.css"
 
 function AboutMe() {
     return (
-        <>
-
-
-        </>
+        <div>
+            <div>
+                <h3>
+                    About Me 
+                </h3>
+                <img src={profile}alt="profile photo" width="400" height="500" className="center"></img>
+                <p> Web developer. Learning about and engaging with new technologies and languages to further my understanding of web development, particulary in front end design.
+                </p>
+                
+                <ul>
+                <p>Tech Stack:</p>
+                    <li>HTML </li><br></br>
+                    <li> CSS </li><br></br>
+                    <li>JavaScript </li><br></br>
+                    <li>JQuery </li><br></br>
+                    <li>Bootstrap</li><br></br>
+                    <li>Express</li><br></br>
+                    <li>Node</li><br></br>
+                    <li>MySql</li><br></br>
+                    <li>Sequelize</li><br></br>
+                    <li>MongoDB</li><br></br>
+                    <li>Mongoose</li><br></br>
+                    <li>HandleBars</li><br></br>
+                    <li>React</li><br></br>
+                    <li>Redux</li><br></br>
+                    <li>MaterialUI</li><br></br>
+                </ul>
+            </div>
+        </div>
     )
 }
 export default AboutMe;
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../css/work.css"
-import firstColor from "../images/peachQuartz.jpg"
-import secondColor from "../images/provence.jpg"
-import thirdColor from "../images/babyBlue.jpg"
-import fourthColor from "../images/classicBlue.jpg"
+import firstImage from "../images/image1.jpg"
+import secondImage from "../images/image2.jpg"
+import thirdImage from "../images/image3.jpg"
+import fourthImage from "../images/image4.jpg"
+import fifthImage from "../images/image5.jpg"
+import sixthImage from "../images/image6.jpg"
+import seventhImage from "../images/image7.jpg"
+import eigthImage from "../images/image8.jpg"
 
 function Work() {
     return (
@@ -12,24 +16,36 @@ function Work() {
                 <div class="row">
                     <div class="col">
                         <a href="https://kembo001.github.io/mealPlan/" target="_blank" rel="noreferrer">
-                            <img src={firstColor} alt="peach quartz color" width="400" height="400"></img>
+                            <img src={firstImage} alt="peach quartz color" width="400" height="400"></img>
                             <p>Recipe Search App</p>
                         </a>
                     </div>
                     <div class="col">
                         <a href="https://github.com/annsabie/bookreviews" target="_blank" rel="noreferrer">
-                            <img src={secondColor} alt="provence blue color" width="400" height="400"></img>
+                            <img src={secondImage} alt="provence blue color" width="400" height="400"></img>
                             <p>allBooks App</p>
                         </a></div>
                     <div class="w-100"></div>
                     <div class="col">
                         <a href="https://emilyelizabethdaniel.github.io/dayPlanner/" target="_blank" rel="noreferrer">
-                            <img src={thirdColor} alt="baby blue color" width="400" height="400"></img>
+                            <img src={thirdImage} alt="baby blue color" width="400" height="400"></img>
                             <p>Day Planner</p>
                         </a>
                     </div>
                     <div class="col">
-                        <img src={fourthColor} alt="classic blue color" width="400" height="400"></img>
+                        <img src={fourthImage} alt="classic blue color" width="400" height="400"></img>
+                    </div>
+                    <div class="col">
+                        <img src={fifthImage} alt="classic blue color" width="400" height="400"></img>
+                    </div>
+                    <div class="col">
+                        <img src={sixthImage} alt="classic blue color" width="400" height="400"></img>
+                    </div>
+                    <div class="col">
+                        <img src={seventhImage} alt="classic blue color" width="400" height="400"></img>
+                    </div>
+                    <div class="col">
+                        <img src={eigthImage} alt="classic blue color" width="400" height="400"></img>
                     </div>
                 </div>
             </div>
